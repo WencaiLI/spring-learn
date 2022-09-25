413ac463c7c68ae568061755ef1bc85441e37173
liwencai <1491269442@qq.com>
2022-09-25 22:23:40 +0800

    拦截器demo

---
+++ springboot-interceptor/src/main/java/com/lwc/springbootinterceptor/Controller/IndexController.java
@@ -0,0 +1,23 @@
+package com.lwc.springbootinterceptor.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/user/account")
+    public String userAccount() {
+        return "/user/account";
+    }
+
+    @RequestMapping("/user/login")
+    public String userLogin() {
+        return "/user/login";
+    }
+
+    @RequestMapping("/user/health")
+    public String health() {
+        return "/user/health";
+    }
+}
